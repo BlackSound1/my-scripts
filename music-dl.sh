c043d50b71a1cc1b62f12d4330b05fe8bca77863
BlackSound1 <matthewr@videotron.ca>
2024-07-27 21:07:43 -0400

    Added help text to music-dl.sh. Simplified logic, too

--- music-dl.sh
+++ music-dl.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
 
+if [ $# -eq 0 ]; then
+    printf "\nUsage: $0 <folder> <URL> [--playlist]\n\n"
+    printf "    folder             Where to save the file(s)\n"
+    printf "    URL                Youtube URL\n"
+    printf "    --playlist         download playlist\n"
+    exit 1
+fi
+
 folder="$1"
 URL="$2"
 
+playlist_flag=""
 if [[ "$3" == "--playlist" ]]; then
-	yt-dlp --extract-audio --yes-playlist --audio-format wav --output "./$folder/%(title)s" $URL
-else 
-	yt-dlp --extract-audio --audio-format wav --output "./$folder/%(title)s" $URL
+    playlist_flag="--yes-playlist"
 fi
 
+yt-dlp --extract-audio $playlist_flag --audio-format wav --output "./$folder/%(title)s" $URL --progress --console-title
